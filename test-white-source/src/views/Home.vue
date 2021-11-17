acd1e4c4207bce42ef9d0fe9d7911b762398e1ea
Hubert Sui <841158840@qq.com>
2021-11-17 19:06:48 +0800

    add echarts

--- test-white-source/src/views/Home.vue
+++ test-white-source/src/views/Home.vue
@@ -2,17 +2,22 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="chartDom"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import echarts from 'echarts';
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
   },
+  mounted(){
+    echarts.init(document.getElementById(chartDom));
+  }
 };
 </script>
